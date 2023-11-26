af4819e0c01e22b5bfc94f221687744d6fdb8f80
kubryak <al.lcn@yandex.ru>
2023-11-26 17:57:48 +0500

    fix: Исправить плюрализацию для чисел больше 100

--- src/store.js
+++ src/store.js
@@ -84,10 +84,14 @@ class Store {
    * @param code
    */
  getSelectionCount(count) {
-  if (count >= 2 && count <= 4) {
-    return 'раза'
+  if (count === 1) {
+    return 'раз';
+  } else if (count % 10 === 1 && count % 100 !== 11) {
+    return 'раз';
+  } else if ((count % 10 >= 2 && count % 10 <= 4) && (count % 100 < 10 || count % 100 >= 20)) {
+    return 'раза';
   } else {
-    return 'раз'
+    return 'раз';
   }
  }
 }
